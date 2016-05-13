ffb46f76136447c46fc54eec32667604e2d623ac
Sebastian Riemschüssel <s.riemschuessel@gmail.com>
2016-05-13 09:58:06 +0200

    adjust test case

--- org.buildingsmart.mvd.tmvd.tests/xtend-gen/org/buildingsmart/mvd/tmvd/tests/AbstractTextualMVDTest.java
+++ org.buildingsmart.mvd.tmvd.tests/xtend-gen/org/buildingsmart/mvd/tmvd/tests/AbstractTextualMVDTest.java
@@ -1,36 +1,29 @@
 package org.buildingsmart.mvd.tmvd.tests;
 
-import com.google.inject.Inject;
 import org.buildingsmart.mvd.mvdxml.MvdXML;
 import org.buildingsmart.mvd.tmvd.TextualMVDInjectorProvider;
 import org.buildingsmart.mvd.tmvd.util.IOHelper;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipse.xtext.junit4.XtextRunner;
-import org.eclipse.xtext.xbase.lib.Extension;
-import org.junit.runner.RunWith;
 
-@RunWith(XtextRunner.class)
+/* @RunWith(XtextRunner.class)
 @InjectWith(TextualMVDInjectorProvider.class)
-@SuppressWarnings("all")
+ */@SuppressWarnings("all")
 public class AbstractTextualMVDTest {
-  @Inject
-  @Extension
-  private IOHelper io;
+  /* @Inject
+   */private IOHelper io;
   
   public MvdXML loadTextualMVD(final String pathToFile) {
     return this.io.loadTextualMVD(pathToFile);
   }
   
-  public EObject loadMvdXML(final String pathToFile) {
+  public Object loadMvdXML(final String pathToFile) {
     return this.io.loadMvdXML(pathToFile);
   }
   
-  public void saveMvdXML(final EObject root, final String pathToFile) {
-    this.io.storeAsMVDXML(root, pathToFile);
+  public Object saveMvdXML(final /* EObject */Object root, final String pathToFile) {
+    return this.io.storeAsMVDXML(root, pathToFile);
   }
   
-  public void saveTextualMVD(final EObject root, final String pathToFile) {
-    this.io.storeAsTMVD(root, pathToFile);
+  public Object saveTextualMVD(final /* EObject */Object root, final String pathToFile) {
+    return this.io.storeAsTMVD(root, pathToFile);
   }
 }
