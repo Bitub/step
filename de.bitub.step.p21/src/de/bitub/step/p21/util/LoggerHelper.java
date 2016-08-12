e5eec0adcd45a8cc455a20804ab65b4c833a60b5
Sebastian Riemschüssel <s.riemschuessel@gmail.com>
2016-08-12 17:53:13 +0200

    check if log folder exists

--- de.bitub.step.p21/src/de/bitub/step/p21/util/LoggerHelper.java
+++ de.bitub.step.p21/src/de/bitub/step/p21/util/LoggerHelper.java
@@ -1,35 +1,38 @@
 package de.bitub.step.p21.util;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class LoggerHelper
-{
-  public static Logger init(Level level, Class<?> forClass)
-  {
-    FileHandler fh;
-    Logger logger = Logger.getLogger(forClass.getName());
+public class LoggerHelper {
+	public static Logger init(Level level, Class<?> forClass) {
+		FileHandler fh;
+		Logger logger = Logger.getLogger(forClass.getName());
 
-    try {
-      // This block configure the logger with handler and formatter
-      //
-      fh = new FileHandler("logs/" + forClass.getSimpleName() + ".log");
-      SimpleFormatter formatter = new SimpleFormatter();
-      fh.setFormatter(formatter);
+		try {
+			// This block configure the logger with handler and formatter
+			//
+			Path logFolderPath = FileSystems.getDefault().getPath("logs");
+			String logFileName = forClass.getSimpleName() + ".log";
 
-      logger.addHandler(fh);
-      logger.setLevel(level);
-      logger.setUseParentHandlers(false);
-    }
-    catch (SecurityException e) {
-      e.printStackTrace();
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
-    return logger;
-  }
+			if (Files.exists(logFolderPath)) {
+				fh = new FileHandler("logs/" + logFileName);
+				SimpleFormatter formatter = new SimpleFormatter();
+				fh.setFormatter(formatter);
+
+				logger.addHandler(fh);
+				logger.setLevel(level);
+				logger.setUseParentHandlers(false);
+			}
+
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
+		return logger;
+	}
 }
